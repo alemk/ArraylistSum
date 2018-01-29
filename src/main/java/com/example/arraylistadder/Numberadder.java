85f08a089802c9c2a3862f759bb1061222cf5236
alemk <alemseged.hailu@gmail.com>
2018-01-29 15:20:32 -0500

    Initial commit

---
+++ src/main/java/com/example/arraylistadder/Numberadder.java
@@ -0,0 +1,31 @@
+package com.example.arraylistadder;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Numberadder {
+
+    public int adder()
+
+    {
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Integer sum = 0;
+        Scanner keyboad = new Scanner(System.in);
+        System.out.println("please enter how many numbers you want to enter");
+        int loopcount = keyboad.nextInt();
+
+        for(int i=0;i< loopcount;i++)
+        {
+           numbers.add(keyboad.nextInt());
+
+            sum += numbers.get(i);
+        }
+        return sum;
+    }
+}
+
+
+
+
+
